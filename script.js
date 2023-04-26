45a6a9dd372a0a1aa668c92495728df0bf7618a1
mohammed-ashiq <mohammed.as@softnotions.com>
2023-04-27 03:03:59 +0530

    Age show in years months and days

--- script.js
+++ script.js
@@ -120,40 +120,69 @@ function validatePhone() {
 
 
 function validateDob() {
-  dob = document.getElementById('contact-dob').value;
 
 
 
-  if (dob.length == 10) {
-    var todayDate = new Date().getTime();
-    var birthDate = new Date(dob).getTime();
-    var age = (todayDate - birthDate) / (1000 * 60 * 60 * 24 * 365)
 
-    var thisMonth = parseInt( new Date().getMonth())
-    var birthMonth = parseInt( new Date(dob).getMonth())
-
-    var month = thisMonth - birthMonth
-
-   
-
-    dobok.innerHTML = "Age is: " + Math.floor(age);
-    
-   
-
-    if (Math.floor(age) >= 18) {
-
-      dobError.innerHTML = 'valid';
-      return true;
-
-    } else {
-      dobError.innerHTML = 'Age should be greater than or equal to 18';
-      return false;
-    }
+  var userinput = document.getElementById("contact-dob").value;
 
 
+
+  //check user provide input or not  (Not required)
+  if (userinput == null || userinput == '') {
+    dobError.innerHTML = "**Choose a date please!";
+    return true;
   }
 
-  return true
+  //execute if user entered a date   
+  else {
+    //extract and collect only date from date-time string  
+    var mdate = userinput.toString();
+    dob = mdate
+    var dobYear = parseInt(mdate.substring(0, 4), 10);
+    var dobMonth = parseInt(mdate.substring(5, 7), 10);
+    var dobDate = parseInt(mdate.substring(8, 10), 10);
+
+    //get the current date from system  
+    var today = new Date();
+    //date string after broking  
+    var birthday = new Date(dobYear, dobMonth - 1, dobDate);
+    console.log(mdate);
+    //calculate the difference of dates  
+    var diffInMillisecond = today.valueOf() - birthday.valueOf();
+
+    //convert the difference in milliseconds and store in day and year variable          
+    var year_age = Math.floor(diffInMillisecond / 31536000000);
+    var day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);
+
+    //when birth date and month is same as today's date        
+    if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {
+      alert("Happy Birthday!");
+      return true
+    }
+
+    var month_age = Math.floor(day_age / 30);
+    day_ageday_age = day_age % 30;
+
+    var tMnt = (month_age + (year_age * 12));
+    var tDays = (tMnt * 30) + day_age;
+
+    //DOB is greater than today's date, generate an error: Invalid date    
+    if (dob > today) {
+      dobError.innerHTML = ("Invalid date input - Please try again!");
+    } else if (year_age < 18) {
+      dobok.innerHTML = "The age is " + year_age + " years " + month_age + " months " + day_age + " days"
+      
+      dobError.innerHTML = ("Age should be greater than or equal to 18!");
+      return false
+    } else {
+
+      dobok.innerHTML = "The age is " + year_age + " years " + month_age + " months " + day_age + " days"
+      dobError.innerHTML = ("Valid");
+      return true
+
+    }
+  }
 
 }
 
